92202fdc52d46fed113a943dbec4d2b9fdac66cb
william wen <wilhelm6939@gmail.com>
2019-03-12 23:27:55 +0800

    add user guest times

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,15 @@
 import random
 r = random.randint(1, 100)
+t = 0
 print(r)
 while True:
 	num = input('請輸入數字: ')
 	num = int(num)
+	t = t + 1
 	if num == r:
 		print('終於猜對了')
+		
+		print('總共猜了', t, '次')
 		break
+
 
